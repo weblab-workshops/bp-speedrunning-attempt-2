8033fea9442635e28599118332982a0193b0d3ae
Nicholas Tsao <ntt1729@gmail.com>
2023-02-18 18:16:08 -0500

    help

--- server/server.js
+++ server/server.js
@@ -18,9 +18,6 @@
 const validator = require("./validator");
 validator.checkSetup();
 
-//allow us to use process.ENV
-require("dotenv").config();
-
 //import libraries needed for the webserver to work!
 const http = require("http");
 const express = require("express"); // backend framework for our node server.
@@ -30,21 +27,18 @@ const path = require("path"); // provide utilities for working with file and dir
 
 const api = require("./api");
 const auth = require("./auth");
-
-// require("dotenv").config();
+require("dotenv").config();
 
 // socket stuff
 const socketManager = require("./server-socket");
 
 // Server configuration below
-// TODO change connection URL after setting up your team database
+// TODO change connection URL after setting up your own database
+// THIS IS HIDDEN IN A DOTENV FILE
 const mongoConnectionURL =
   "mongodb+srv://weblab:jAT4po55IAgYWQgR@catbook-ylndp.mongodb.net/test?retryWrites=true&w=majority";
 // TODO change database name to the name you chose
 const databaseName = "bp-speedrun";
-
-// mongoose 7 warning
-mongoose.set("strictQuery", false);
 
 // connect to mongodb
 mongoose
@@ -60,13 +54,9 @@ mongoose
 const app = express();
 app.use(validator.checkRoutes);
 
-// allow us to process POST requests
-app.use(express.json());
-
 // set up a session, which will persist login data across requests
 app.use(
   session({
-    // TODO: add a SESSION_SECRET string in your .env file, and replace the secret with process.env.SESSION_SECRET
     secret: "session-secret",
     resave: false,
     saveUninitialized: false,
